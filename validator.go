97f9ebea2ef2f99b5c3db0535d8c2bf9e1cdcefc
Paul Smith <paulsmith@pobox.com>
2015-05-12 03:38:25 -0500

    Fix some stuff and add example

--- validator.go
+++ validator.go
@@ -14,18 +14,18 @@ import (
 
 func main() {
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s <plans_schema.json>\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Usage: %s <plans_schema.json> <providers_schema.json> <drugs_schema.json>\n", os.Args[0])
 		flag.PrintDefaults()
 	}
 
 	flag.Parse()
 
-	if flag.NArg() < 1 {
+	if flag.NArg() != 3 {
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	validator, err := NewValidator(flag.Arg(0))
+	validator, err := NewValidator(flag.Arg(0), flag.Arg(1), flag.Arg(2))
 	if err != nil {
 		log.Fatalln("new validator:", err.Error())
 	}
@@ -46,19 +46,36 @@ func main() {
 		}
 		http.ServeFile(w, r, fn)
 	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	port := "8080"
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 type Validator struct {
 	schemas map[string]*js.Schema
 }
 
-func NewValidator(plansSchemaPath string) (*Validator, error) {
+func NewValidator(plans, providers, drugs string) (*Validator, error) {
 	v := new(Validator)
 	v.schemas = make(map[string]*js.Schema)
 	var err error
-	v.schemas["plans"], err = js.NewSchema(js.NewReferenceLoader("file://" + abs(plansSchemaPath)))
-	return v, err
+	for _, x := range []struct {
+		name     string
+		filename string
+	}{
+		{"plans", plans},
+		{"providers", providers},
+		{"drugs", drugs},
+	} {
+		v.schemas[x.name], err = js.NewSchema(js.NewReferenceLoader("file://" + abs(x.filename)))
+		if err != nil {
+			log.Printf(x.name)
+			return nil, err
+		}
+	}
+	return v, nil
 }
 
 func (v *Validator) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -66,6 +83,11 @@ func (v *Validator) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		errors []string
 		valid  bool
 	)
+	jsonDoc := r.FormValue("json")
+	if r.FormValue("try") == "1" {
+		r.Method = "POST"
+		jsonDoc = plansExample
+	}
 	if r.Method == "POST" {
 		// Validate JSON
 		schema, ok := v.schemas[r.FormValue("doc-type")]
@@ -73,7 +95,7 @@ func (v *Validator) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			errors = []string{fmt.Sprintf("This document type schema not yet implemented: %q", r.FormValue("doc-type"))}
 			goto render
 		}
-		loader := js.NewStringLoader(r.FormValue("json"))
+		loader := js.NewStringLoader(jsonDoc)
 		result, err := schema.Validate(loader)
 		if err != nil {
 			errors = []string{
@@ -103,7 +125,11 @@ render:
     <body>
 		<div class="container">
 			<h1>QHP provider and formulary JSON Schema validator</h1>
-			<p><a href="https://github.com/CMSgov/QHP-provider-formulary-APIs">Click here for more information.</a></p>
+			<p>
+				<a href="https://github.com/CMSgov/QHP-provider-formulary-APIs">Click here for more information</a>
+				|
+				<a href="?try=1&amp;doc-type=plans">Try an example</a>
+			</p>
 			{{if .Errors}}
 				<div class="text-danger">
 					<p>This document is <b>not valid</b>:</p>
@@ -123,13 +149,13 @@ render:
 			<pre>{{.JSON}}</pre>
 			<hr>
 			{{end}}
-			<form method="post">
+			<form method="post" action="./">
 				<div class="form-group">
 					<label for="docType">JSON document type</label>
 					<select class="form-control" id="docType" name="doc-type" aria-describedby="docTypeHelp">
 						<option value="plans" selected>Plans</option>
-						<option value="providers" disabled>Providers</option>
-						<option value="drugs" disabled>Drugs</option>
+						<option value="providers">Providers</option>
+						<option value="drugs">Drugs</option>
 					</select>
 					<span id="docTypeHelp" class="help-block">The type of JSON document to be validated.</span>
 				</div>
@@ -161,7 +187,7 @@ render:
 		errors,
 		valid,
 		r.Method,
-		r.FormValue("json"),
+		jsonDoc,
 	}); err != nil {
 		log.Printf("rendering html: %v", err)
 		http.Error(w, http.StatusText(500), 500)
@@ -175,3 +201,67 @@ func abs(path string) string {
 	}
 	return p
 }
+
+const plansExample = `
+[
+    {
+        "plan_id_type": "HIOS-PLAN-ID",
+        "plan_id": "12345XX9876543",
+        "marketing_name": "Sample Gold Health Plan",
+        "summary_url": "http://url/to/summary/benefits/coverage",
+        "marketing_url": "http://url/to/health/plan/information",
+        "plan_contact": "email@address.com",
+        "network": [
+            {
+                "network_tier": "PREFERRED"
+            },
+            {
+                "network_tier": "NON-PREFERRED"
+            }
+        ],
+        "formulary": [
+            {
+                "drug_tier": "GENERIC",
+                "mail_order": true,
+                "cost_sharing": [
+                    {
+                        "pharmacy_type": "1-MONTH-IN-RETAIL",
+                        "copay_amount": 20.0,
+                        "copay_opt": "AFTER-DEDUCTIBLE",
+                        "coinsurance_rate": 0.10,
+                        "coinsurance_opt": "BEFORE-DEDUCTIBLE"
+                    },
+                    {
+                        "pharmacy_type": "1-MONTH-IN-MAIL",
+                        "copay_amount": 0.0,
+                        "copay_opt": "NO-CHARGE",
+                        "coinsurance_rate": 0.20,
+                        "coinsurance_opt": null
+                    }
+                ]
+            },
+            {
+                "drug_tier": "BRAND",
+                "mail_order": true,
+                "cost_sharing": [
+                    {
+                        "pharmacy_type": "1-MONTH-IN-RETAIL",
+                        "copay_amount": 15.0,
+                        "copay_opt": null,
+                        "coinsurance_rate": 0.0,
+                        "coinsurance_opt": null
+                    },
+                    {
+                        "pharmacy_type": "1-MONTH-IN-MAIL",
+                        "copay_amount": 20.0,
+                        "copay_opt": "AFTER-DEDUCTIBLE",
+                        "coinsurance_rate": 0.10,
+                        "coinsurance_opt": "BEFORE-DEDUCTIBLE"
+                    }
+                ]
+            }
+        ],
+        "last_updated_on": "2015-03-17"
+    }
+]
+`
