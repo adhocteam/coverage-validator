6df699b141d719965140cff0e1a99901956d63a8
James Rhein <james@adhocteam.us>
2016-07-21 10:37:52 -0500

    [MRKTPLCAPI-46] Add support for file uploads and streaming validation
    This change makes this project depend on the marketplace-api* project,
    specifically on the coverage package therein, beginning with commit
    95ab3f7.
    
    * https://github.com/CMSgov/marketplace-api
    
    Change-Id: I4b110a67fd4bb6ca5c2709363b3bd028896c26df

--- validator.go
+++ validator.go
@@ -1,50 +1,83 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
-	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"net"
 	"net/http"
 	"os"
-	"path/filepath"
+	"strings"
+	"time"
 
+	"github.com/CMSgov/marketplace-api/marketplace/adapters"
+	"github.com/CMSgov/marketplace-api/marketplace/coverage"
+	"github.com/CMSgov/marketplace-api/marketplace/drivers"
+
+	log "github.com/Sirupsen/logrus"
 	js "github.com/xeipuuv/gojsonschema"
 )
 
+var (
+	logger       *log.Logger
+	providerRepo adapters.DBProviderRepo
+)
+
 func main() {
-	var (
-		plansSchema     = flag.String("plans", "plans_schema.json", "plans JSON schema")
-		providersSchema = flag.String("providers", "providers_schema.json", "providers JSON schema")
-		drugsSchema     = flag.String("drugs", "drugs_schema.json", "drugs JSON schema")
-		indexSchema     = flag.String("index", "index_schema.json", "index JSON schema")
-	)
-
-	flag.Parse()
-
-	validator := NewValidator()
-
-	for _, s := range []struct {
-		name, filename string
-	}{
-		{"plans", *plansSchema},
-		{"providers", *providersSchema},
-		{"drugs", *drugsSchema},
-		{"index", *indexSchema},
-	} {
-		f, err := os.Open(s.filename)
-		if err != nil {
-			log.Fatalf("opening %s schema from file %s: %v", s.name, s.filename, err)
-		}
-		if err := validator.Add(s.name, f); err != nil {
-			log.Fatalf("adding %s schema from file %s: %v", s.name, s.filename)
-		}
-		f.Close()
+	dbConnectionInfo := os.Getenv("DB_CONN_INFO")
+	logger = &log.Logger{
+		Out:       os.Stderr,
+		Formatter: &log.TextFormatter{FullTimestamp: true},
+		Level:     log.InfoLevel,
 	}
+
+	dbUp := make(chan error)
+	const maxDbAttempts = 2
+	dbconn := func(conninfo string, done chan<- error) (*drivers.PostgresHandler, error) {
+		var dbErr error
+
+		handler, err := drivers.NewPostgresHandler(dbConnectionInfo, logger)
+		if err != nil {
+			return nil, err
+		}
+
+		go func() {
+			for i := 0; i < maxDbAttempts; i++ {
+				time.Sleep(time.Duration(i) * time.Second)
+				if dbErr = handler.TestConnection(); dbErr != nil {
+					logger.WithError(dbErr).Error("db connection error")
+					continue
+				}
+				break
+			}
+			dbUp <- dbErr
+		}()
+
+		return handler, nil
+	}
+
+	dbHandler, err := dbconn(dbConnectionInfo, dbUp)
+	if err != nil {
+		panic(err)
+	}
+
+	waitDbUp := func(dbUp <-chan error) {
+		if err = <-dbUp; err != nil {
+			fmt.Printf("err = %+v\n", err)
+			logger.WithError(err).Fatalf("couldn't connect to db, giving up")
+		}
+	}
+
+	waitDbUp(dbUp)
+
+	dbHandlers := make(map[string]adapters.DBHandler)
+	dbHandlers["DBProviderRepo"] = dbHandler
+
+	providerRepo = *adapters.NewDBProviderRepo(dbHandlers)
+	validator := NewValidator()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "index.html")
@@ -101,13 +134,28 @@ func (v Validator) Add(name string, r io.Reader) error {
 
 var ErrSchemaUnknown = errors.New("validator: unknown schema")
 
-func (v Validator) Validate(schemaName string, jsonDoc string) (*js.Result, error) {
-	schema, ok := v[schemaName]
-	if !ok {
-		return nil, ErrSchemaUnknown
+func (v Validator) Validate(schemaName string, jsonDoc io.Reader) error {
+	switch schemaName {
+	case "providers":
+		validator := &coverage.StreamingProviderValidator{
+			Dec: json.NewDecoder(jsonDoc),
+		}
+		return validator.Valid(&providerRepo)
+
+	case "drugs":
+		validator := coverage.NewStreamingDrugValidator(jsonDoc)
+		return validator.Valid()
+	case "index":
+		validator := coverage.NewIndexDocValidator(jsonDoc)
+		return validator.Validate()
+	case "plans":
+		validator := coverage.NewStreamingPlanValidator(jsonDoc)
+		return validator.Valid()
+	default:
+		return ErrSchemaUnknown
 	}
-	loader := js.NewStringLoader(jsonDoc)
-	return schema.parsed.Validate(loader)
+
+	return nil
 }
 
 func (v Validator) ServeFile(schemaName string, w http.ResponseWriter) {
@@ -129,45 +177,85 @@ func (v Validator) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	var resp ValidationResult
+
+	if strings.HasPrefix(r.Header.Get("Content-Type"), "multipart/form-data") {
+		resp = multipartFormValidate(v, w, r)
+
+	} else {
+		jsonDoc := r.FormValue("json")
+		err := v.Validate(r.FormValue("schema"), bytes.NewBufferString(jsonDoc))
+		if err != nil {
+			resp.Valid = false
+			renderError(w, &resp, err)
+		} else {
+			resp.Valid = true
+		}
+	}
+
+	if resp.Valid {
+		if err := json.NewEncoder(w).Encode(resp); err != nil {
+			http.Error(w, http.StatusText(500), 500)
+		}
+	}
+}
+
+func multipartFormValidate(v Validator, w http.ResponseWriter, r *http.Request) ValidationResult {
+	var resp ValidationResult
+
+	reader, err := r.MultipartReader()
+	if err != nil {
+		logger.Errorf("There was an error: %s\n", err)
+	}
+
+	for {
+		part, err := reader.NextPart()
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				logger.Errorf("There was an error: %s\n", err)
+			}
+		}
+
+		if part.FormName() == "schema" {
+			buff, err := ioutil.ReadAll(part)
+			if err != nil {
+				logger.Errorf("Error reading schema name - %+v\n", err)
+			}
+			resp.Schema = string(buff)
+		}
+		if part.FormName() == "json" {
+			err = v.Validate(resp.Schema, part)
+			if err != nil {
+				renderError(w, &resp, err)
+			} else {
+				resp.Valid = true
+			}
+		}
+	}
+	return resp
+}
+
+func renderError(w http.ResponseWriter, resp *ValidationResult, err error) {
 	render := func() {
 		if err := json.NewEncoder(w).Encode(resp); err != nil {
 			http.Error(w, http.StatusText(500), 500)
 		}
 	}
-	jsonDoc := r.FormValue("json")
-	schemaName := r.FormValue("schema")
-	resp.Schema = schemaName
-	result, err := v.Validate(schemaName, jsonDoc)
+
 	if err != nil {
 		if err == ErrSchemaUnknown {
-			resp.Errors = []string{fmt.Sprintf("This schema is unknown: %q", r.FormValue("schema"))}
+			resp.Errors = []string{fmt.Sprintf("This schema is unknown: %q", resp.Schema)}
 			render()
-			return
 		}
 		resp.Errors = []string{"JSON is not well-formed: " + err.Error()}
-	} else {
-		if result.Valid() {
-			resp.Valid = true
-		} else {
-			for _, err := range result.Errors() {
-				msg := err.Context.String() + ": " + err.Description
-				resp.Errors = append(resp.Errors, msg)
-			}
-		}
+		render()
 	}
-	render()
+
 }
 
 type ValidationResult struct {
 	Valid  bool     `json:"valid"`
 	Errors []string `json:"errors"`
 	Schema string   `json:"schema"`
-}
-
-func abs(path string) string {
-	p, err := filepath.Abs(path)
-	if err != nil {
-		panic(err)
-	}
-	return p
 }
